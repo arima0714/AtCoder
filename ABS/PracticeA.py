737594361333d18248b71502562c9b6cac6dba32
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-04 10:57:17 +0900

    PracticeAはACした

--- ABS/PracticeA.py
+++ ABS/PracticeA.py
@@ -5,6 +5,11 @@ line_1 = input()
 tmp_str = input()
 line_2 = tmp_str.split()
 line_3 = input()
-print(line_1)
-print(line_2)
-print(line_3)
+# print(line_1)
+# print(line_2)
+# print(line_3)
+
+integer = int(line_1) + int(line_2[0])+ int(line_2[1])
+string  = line_3
+
+print("%d %s" % (integer, string))
