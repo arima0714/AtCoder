c61aeffe5e066cf5ebbdca86fcaf0a3c2056d0f7
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-04 10:58:28 +0900

    不要なコメントを削除した

--- ABS/PracticeA.py
+++ ABS/PracticeA.py
@@ -1,13 +1,7 @@
-# s = [input() for i in range(3)]
-# print(s)
-
 line_1 = input()
 tmp_str = input()
 line_2 = tmp_str.split()
 line_3 = input()
-# print(line_1)
-# print(line_2)
-# print(line_3)
 
 integer = int(line_1) + int(line_2[0])+ int(line_2[1])
 string  = line_3
