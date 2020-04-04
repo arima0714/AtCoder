4bd070be358c78487d1019bdd5180e1aecf3e9fd
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-04 10:51:08 +0900

    入力を希望の方法で取得できるようになった

--- ABS/PracticeA.py
+++ ABS/PracticeA.py
@@ -1,2 +1,10 @@
-s = [input() for i in range(3)]
-print(s)
+# s = [input() for i in range(3)]
+# print(s)
+
+line_1 = input()
+tmp_str = input()
+line_2 = tmp_str.split()
+line_3 = input()
+print(line_1)
+print(line_2)
+print(line_3)
