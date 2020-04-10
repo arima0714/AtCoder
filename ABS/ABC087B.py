7d35acf17a0ca9ee968b105c002daa49e4cf156a
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-10 19:24:44 +0900

    元の個数が0の場合に対応できるように修正した

--- ABS/ABC087B.py
+++ ABS/ABC087B.py
@@ -3,11 +3,18 @@ num100 = int(input())
 num50  = int(input())
 num_sum= int(input())
 
+if num500 != 0:
+    num500 = num500+1
+if num100 != 0:
+    num100 = num100+1
+if num50 != 0:
+    num50 = num50+1
+
 answer = 0
 
-for n500 in range(num500 + 1):
-    for n100 in range(num100 + 1):
-        for n50 in range(num50 + 1):
+for n500 in range(num500):
+    for n100 in range(num100):
+        for n50 in range(num50):
             if 500 * n500 + 100 * n100 + 50 * n50 == num_sum:
                 answer = answer + 1
 
