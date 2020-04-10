9a53622fcf065a1ed50a4f8f0d5a841d894d0566
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-10 19:01:01 +0900

    loop変数を導入

--- ABS/ABC081B.py
+++ ABS/ABC081B.py
@@ -3,12 +3,14 @@ input_line = input().split()
 input_line = [int(s) for s in input_line]
 
 counter = 0
+loop = True
 
 while True:
     for k in input_line:
         if k % (2 ** counter) == False:
+            loop = False
             break
         break
     break
 
-print(counter-1)
+print(counter)
