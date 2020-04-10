08938a4e4e9e30ac1b662741f302f2750a752bdd
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-10 18:44:04 +0900

    返値が0の場合に対処できていない

--- ABS/ABC081B.py
+++ ABS/ABC081B.py
@@ -1,5 +1,17 @@
 input_num = int(input())
 input_line = input().split()
+input_line = [int(s) for s in input_line]
+
+counter = 0
+
+while True:
+    for k in input_line:
+        if k % (2 ** counter) == False:
+            break
+        break
+    break
+
+print(counter-1)
 
 print(input_num)
 print(input_line)
