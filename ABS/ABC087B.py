191da5496c91a92c2dd48d89219382be6d81d48d
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-10 19:21:46 +0900

    探索する範囲が1少なかったのでそれを修正

--- ABS/ABC087B.py
+++ ABS/ABC087B.py
@@ -5,9 +5,9 @@ num_sum= int(input())
 
 answer = 0
 
-for n500 in range(num500):
-    for n100 in range(num100):
-        for n50 in range(num50):
+for n500 in range(num500 + 1):
+    for n100 in range(num100 + 1):
+        for n50 in range(num50 + 1):
             if 500 * n500 + 100 * n100 + 50 * n50 == num_sum:
                 answer = answer + 1
 
