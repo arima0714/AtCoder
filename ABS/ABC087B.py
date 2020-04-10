c0e9952b49c3de18b47ad0c7e82d2d8fca4e0fee
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-10 19:18:53 +0900

    全探索に当たってのfor文の外郭を作った

---
+++ ABS/ABC087B.py
@@ -0,0 +1,16 @@
+num500 = int(input())
+num100 = int(input())
+num50  = int(input())
+num_sum= int(input())
+
+answer = 0
+
+for n500 in range(num500):
+    for n100 in range(num100):
+        for n50 in range(num50):
+            pass
+
+print(num500)
+print(num100)
+print(num50)
+print(num_sum)
