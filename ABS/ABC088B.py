7be2189df2f47b2755fa05a8ba50b01a8a7b9891
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-11 14:47:35 +0900

    題意を満たすコードをついかした

--- ABS/ABC088B.py
+++ ABS/ABC088B.py
@@ -1,11 +1,18 @@
 # １行目 [数値]
 # ２行目 [数値1 数値2 数値3 ...] のように１行目の個数分だけ数値が入力される
 
-input_number = input()
+input_number = int(input())
 input_line   = list(input().split())
 input_line = [int(s) for s in input_line]
 input_line.sort(reverse=True)
 
+alice   = 0
+bob     = 0
 
+for i in list(range(input_number)):
+    if i % 2 == 0:
+        alice = alice + input_line[i]
+    else:
+        bob = bob + input_line[i]
 
-print(input_line)
+print(alice - bob)
