4d6d00bfbe233459e60ddad712d63a4023cf9992
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-11 14:44:52 +0900

    入力に関するメモを追加した

--- ABS/ABC088B.py
+++ ABS/ABC088B.py
@@ -1,6 +1,11 @@
+# １行目 [数値]
+# ２行目 [数値1 数値2 数値3 ...] のように１行目の個数分だけ数値が入力される
+
 input_number = input()
 input_line   = list(input().split())
 input_line = [int(s) for s in input_line]
 input_line.sort(reverse=True)
+
+
 
 print(input_line)
