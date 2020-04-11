278db69729775dc3a6ad00df65ca4c6728f6e88e
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-11 14:23:31 +0900

    不要な記述を削除し、題意を満たすコードを記述した

--- ABS/ABC083B.py
+++ ABS/ABC083B.py
@@ -1,13 +1,13 @@
 input_line = input().split()
 
 answer = 0
-tmp = 0
 
 for i in range(int(input_line[0]) + 1) :
+    tmp = 0
     for j in list(str(i)):
-        print("str")
-        print(j)
-    print(i)
-
-
-print(input_line)
+        tmp = tmp + int(j)
+    if int(input_line[1]) <=  tmp <= int(input_line[2]) :
+        print("tmp = ", i)
+        answer = answer + i
+        
+print(answer)
