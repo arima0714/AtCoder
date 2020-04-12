e703b8110e4073b2667e78c3ab838973a5648d46
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-12 13:43:48 +0900

    題意を満たすコードを追加した

--- ABS/ABC085C.py
+++ ABS/ABC085C.py
@@ -3,10 +3,17 @@
 input_line = input().split()
 input_line = [int(s) for s in input_line]
 
-x = -1
-y = -1
-z = -1
+loop = True
 
-
-
-print(str(x) +" "+ str(y) +" "+ str(z))
+for i in range(input_line[0]):
+    for j in range(input_line[0]):
+        for k in range(input_line[0]):
+            if(input_line[1] == 10000 * i + 5000 * j + 1000 * k and i + j + k == input_line[0]):
+                print(str(i) +" "+ str(j) +" "+ str(k))
+                loop = False
+            elif (loop == False):
+                break
+        if (loop == False):
+            break
+    if (loop == False):
+        break
