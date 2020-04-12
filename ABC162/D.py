ba704aba679df6cf931f57cdb6c976ec2637aa8b
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-12 22:27:21 +0900

    とりあえずcommit

--- ABC162/D.py
+++ ABC162/D.py
@@ -8,8 +8,15 @@ result = 0
 for k in range(input_nums):
     for j in range(k):
         for i in range(j):
-            if(j-i != k-j):
-                if(input_line[i] != input_line[j] and input_line[i] != input_line[k] and input_line[j] != input_line[k]):
-                    result = result + 1
+            if(j-i == k-j):
+                pass
+            elif(input_line[i] == input_line[j]):
+                pass
+            elif(input_line[i] == input_line[k]):
+                pass
+            elif(input_line[j] == input_line[k]):
+                pass
+            else:
+                result = result + 1
 
 print(result)
