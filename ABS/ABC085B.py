2859b1b12fd62acfbc30b5c9eec921da4e591d30
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-12 11:32:57 +0900

    入力以外の部分で題意を満たすコードを追加した

--- ABS/ABC085B.py
+++ ABS/ABC085B.py
@@ -1,6 +1,11 @@
+# 一行目は[数値]
+# 二行目以降は一行目の数値の数だけ[数値]
+
 input_number = input()
 input_line = list(input().split())
+input_line = [int(s) for s in input_line]
 
-input_line.sort().sorted()
+input_line.sort(reverse=True)
+input_line = list(dict.fromkeys(input_line))
 
-print(input_line)
+print(len(input_line))
