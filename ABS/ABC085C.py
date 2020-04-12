d30c486082364e1e7626f8ac620ba498c1f1e1bf
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-12 13:49:00 +0900

    ループを一つ消した

--- ABS/ABC085C.py
+++ ABS/ABC085C.py
@@ -7,13 +7,9 @@ loop = True
 
 for i in range(input_line[0]):
     for j in range(input_line[0]):
-        for k in range(input_line[0]):
-            if(input_line[1] == 10000 * i + 5000 * j + 1000 * k and i + j + k == input_line[0]):
-                print(str(i) +" "+ str(j) +" "+ str(k))
-                loop = False
-            elif (loop == False):
-                break
-        if (loop == False):
-            break
+        k = input_line[0] - i -j
+        if(input_line[1] == 10000 * i + 5000 * j + 1000 * k and i + j + k == input_line[0]):
+            print(str(i) +" "+ str(j) +" "+ str(k))
+            loop = False
     if (loop == False):
         break
