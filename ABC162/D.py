f2ceaeab2a402696ca4e2f4b5427fc9bfc17567f
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-12 21:53:16 +0900

    妥当なコードを追加した

--- ABC162/D.py
+++ ABC162/D.py
@@ -1,7 +1,15 @@
+# 入力は二行でそれぞれ[数値]
+
 input_nums = int(input())
 input_line = input()
+
+result = 0
 
 for k in range(input_nums):
     for j in range(k):
         for i in range(j):
-            if(i > j or j > k):
+            if(j-i != k-j):
+                if(input_line[i] != input_line[j] and input_line[i] != input_line[k] and input_line[j] != input_line[k]):
+                    result = result + 1
+
+print(result)
