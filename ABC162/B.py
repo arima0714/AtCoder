acc9966f90c4873e07598ced560917a638037867
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-12 21:19:48 +0900

    入力に関するメモを記述し、題意を満たすコードを追加した

--- ABC162/B.py
+++ ABC162/B.py
@@ -1,3 +1,15 @@
+# 入力は一行のみで[数値]
+
 input_num = int(input())
 
-print(input_num)
+answer = 0
+
+for i in range(input_num):
+    if i%3 == 0:
+        pass
+    elif i%5 == 0:
+        pass
+    else:
+        answer = answer + i
+
+print(answer)
