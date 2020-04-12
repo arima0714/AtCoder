e7f73b012aa0db1cd365418b05372ad2c02afd89
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-12 21:35:46 +0900

    処理時間が長そうだが、題意を満たすコードを追加した

--- ABC162/C.py
+++ ABC162/C.py
@@ -2,10 +2,21 @@
 
 input_num = int(input())
 
-import math
-from functools import reduce
-
 def gcd(*numbers):
     return reduce(math.gcd, numbers)
 
-print(gcd(27, 18, 9))
+import math
+from functools import reduce
+
+sum = 0
+
+for i in range(input_num):
+    i = i+1
+    for j in range(input_num):
+        j = j+1
+        for k in range(input_num):
+            k = k+1
+#            print("i = "+str(i)+" j = "+str(j)+" k = "+str(k)+" gcd() = "+str(gcd(i,j,k)))
+            sum = sum + gcd(i, j, k)
+
+print(sum)
