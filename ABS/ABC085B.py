676164186b6b21ae533f5a7be9db982622240910
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-12 11:36:03 +0900

    題意を満たすコードを追加した

--- ABS/ABC085B.py
+++ ABS/ABC085B.py
@@ -1,9 +1,12 @@
 # 一行目は[数値]
 # 二行目以降は一行目の数値の数だけ[数値]
 
-input_number = input()
-input_line = list(input().split())
-input_line = [int(s) for s in input_line]
+input_number = int(input())
+
+input_line = []
+
+for i in range(input_number):
+    input_line.append(int(input()))
 
 input_line.sort(reverse=True)
 input_line = list(dict.fromkeys(input_line))
