13f86c2ca1e8421868a311533dda5802dfb2b418
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-13 09:25:19 +0900

    解説と同様のものを実装したが時間がかかる

--- ABC162/C.py
+++ ABC162/C.py
@@ -1,16 +1,8 @@
 # 入力は一行のみで[数値]
+import math
 
 input_num = int(input())
 
-def gcd(*numbers):
-    return reduce(math.gcd, numbers)
-
-import math
-from functools import reduce
-
-memory = {}
-tmp_str = ""
-tmp_list = [0, 0, 0]
 
 sum = 0
 
@@ -20,13 +12,6 @@ for i in range(input_num):
         j = j+1
         for k in range(input_num):
             k = k+1
-            tmp_list[0] = i
-            tmp_list[1] = j
-            tmp_list[2] = k
-            tmp_list.sort()
-            tmp_str = str(tmp_list)
-            if(memory.get(tmp_str) == None):
-                memory[tmp_str] = gcd(i,j,k)
-            sum = sum + memory[tmp_str]
+            sum = sum + math.gcd(math.gcd(i,j),k)
 
 print(sum)
