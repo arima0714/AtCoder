27ac68df871e0c34aa8ad2fca7fc371fd965c0a7
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-15 11:32:44 +0900

    入力に関するメモを追加した

--- ABS/ABC049C.py
+++ ABS/ABC049C.py
@@ -1,3 +1,5 @@
+# 入力は一行のみで[文字列]
+
 S = input()
 
 dream = "dream"
@@ -13,16 +15,12 @@ while loop == True:
     if(len(S) == 0):
         break
     if(S[-1] == "m" and S[-5:] == dream):
-        print("last word is dream")
         S = S[0:len(S)-len(dream)]
     elif(S[-1] == "e" and S[-5:] == erase):
-        print("last word is erase")
         S = S[0:len(S)-len(erase)]
     elif(S[-3:] == "ser" and S[-6:] == eraser):
-        print("last word is eraser")
         S = S[0:len(S)-len(eraser)]
     elif(S[-3:] == "mer" and S[-7:] == dreamer):
-        print("last word is dreamer")
         S = S[0:len(S)-len(dreamer)]
     elif(len(S) != 0):
         answer = "NO"
