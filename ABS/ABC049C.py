9ee51d68d8e7a9980f0a4bdf9a6b5e2a9f28be9b
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-15 11:26:42 +0900

    不要な出力はあるがテストケースは通過した

--- ABS/ABC049C.py
+++ ABS/ABC049C.py
@@ -5,21 +5,28 @@ dreamer = "dreamer"
 erase = "erase"
 eraser = "eraser"
 
+loop = True
+
 answer = "YES"
 
-if(S[-1] == "m" and S[-5:] == dream):
-    print("last word is dream")
-    S = S[0:len(S)-len(dream)]
-elif(S[-1] == "e" and S[-5:] == erase):
-    print("last word is erase")
-    S = S[0:len(S)-len(dreamer)]
-elif(S[-3:] == "ser" and S[-6:] == eraser):
-    print("last word is eraser")
-    S = S[0:len(S)-len(erase)]
-elif(S[-3:] == "mer" and S[-7:] == dreamer):
-    print("last wor d is dreamer")
-    S = S[0:len(S)-len(eraser)]
-elif(len(S) != 0):
-    answer = "NO"
+while loop == True:
+    if(len(S) == 0):
+        break
+    if(S[-1] == "m" and S[-5:] == dream):
+        print("last word is dream")
+        S = S[0:len(S)-len(dream)]
+    elif(S[-1] == "e" and S[-5:] == erase):
+        print("last word is erase")
+        S = S[0:len(S)-len(erase)]
+    elif(S[-3:] == "ser" and S[-6:] == eraser):
+        print("last word is eraser")
+        S = S[0:len(S)-len(eraser)]
+    elif(S[-3:] == "mer" and S[-7:] == dreamer):
+        print("last word is dreamer")
+        S = S[0:len(S)-len(dreamer)]
+    elif(len(S) != 0):
+        answer = "NO"
+        loop = False
+        break
 
 print(answer)
