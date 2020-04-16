736fbab2a82778054e03faa14c918403ea64c078
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-16 23:43:15 +0900

    二行目以降の入力を数値のリストとして入力をまとめたリストに追加するようにした

--- ABS/ABC086C.py
+++ ABS/ABC086C.py
@@ -1,3 +1,10 @@
 N = int(input())
+
+inputs = []
+
 for i in range(N):
-    input()
+    input_line = input().split()
+    input_line = [int(s) for s in input_line]
+    inputs.append(input_line)
+    print(input_line)
+
