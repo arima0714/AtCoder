2bc3bbf35d9ecf781c5a8d16bec982bfe8580a84
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-17 11:42:51 +0900

    不要な出力もあるが題意を満たすコードを追加した

--- ABS/ABC086C.py
+++ ABS/ABC086C.py
@@ -1,10 +1,25 @@
 N = int(input())
 
 inputs = []
+t = 0
+x = 0
+y = 0
+answer = "Yes"
 
 for i in range(N):
     input_line = input().split()
     input_line = [int(s) for s in input_line]
+    t = input_line[0]
+    x = input_line[1]
+    y = input_line[2]
     inputs.append(input_line)
-    print(input_line)
+    if ( t < x + y):
+        print("No -> t < x + y")
+        answer = "No"
+    elif ( (x+y)%2 != t%2):
+        print("No -> (x+y)%2 != t%2")
+        answer = "No"
+    else:
+        pass    # YES
 
+print(answer)
