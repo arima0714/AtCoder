0577edce3d8fdfd76200114e40883257b2409020
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-23 14:59:55 +0900

    微妙にずれが生じてしまっている

--- ABC163/D.py
+++ ABC163/D.py
@@ -1,9 +1,18 @@
 # 入力は1行で[数値 数値]
 
 il1 = input().split()
-N = int(il1[0])
+N = int(il1[0])+1
 K = int(il1[1])
 
 list = [1]
 for i in range(1,N+1):
     list.append(list[-1] * i)
+
+answer = 0
+
+for k in range(K, N+1):
+    print("N= "+str(N)+" N-k= "+str(N-k)+" k= "+str(k))
+    answer = answer + list[N] / (list[N-k] * list[k])
+
+print(answer)
+print(list)
