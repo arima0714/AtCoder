9bdef7b8b843097347d66b1f519e3246844e2c71
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-23 16:03:19 +0900

    3つ目の入力例を間違えている

--- ABC163/D.py
+++ ABC163/D.py
@@ -1,6 +1,6 @@
 # 入力は1行で[数値 数値]
 def return_number(n, k):
-    return n * k - k**2 + k + 1
+    return n * k - k*k + k + 1
 
 
 il1 = input().split()
@@ -12,7 +12,7 @@ min_ = 0
 max_ = 0
 
 for k in range(K, N+2):
-    num  = return_number(n, k)
+    num  = return_number(N, k)
     answer = answer + num
 
 print(answer)
