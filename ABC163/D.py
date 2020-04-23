6100701f7c04bd01c441da4283ae9f502b27fe6d
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-23 15:59:43 +0900

    for文を多用しすぎているので遅い

--- ABC163/D.py
+++ ABC163/D.py
@@ -1,19 +1,7 @@
 # 入力は1行で[数値 数値]
-def return_number(max, min):
-    return max-min+1
+def return_number(n, k):
+    return n * k - k**2 + k + 1
 
-def return_max(n, k):
-    ans = 0
-    for i in range(k):
-        ans = ans + n - i
-        # print("ans= "+str(ans)+" i= "+str(i))
-    return ans
-
-def return_min(n, k):
-    ans = 0
-    for i in range(k):
-        ans = ans + i
-    return ans
 
 il1 = input().split()
 N = int(il1[0])
@@ -24,10 +12,7 @@ min_ = 0
 max_ = 0
 
 for k in range(K, N+2):
-    min_ = return_min(N,k)
-    max_ = return_max(N,k)
-    num  = return_number(max_, min_)
-    # print("k= "+str(k)+" N= " +str(N)+" min= "+str(min_)+" max= "+str(max_)+" num= "+str(num))
+    num  = return_number(n, k)
     answer = answer + num
 
 print(answer)
