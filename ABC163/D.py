a3fd35d74428cbbc2d9ad614116ebb64ffffdaa1
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-23 14:24:20 +0900

    入力のメモを追加した

--- ABC163/D.py
+++ ABC163/D.py
@@ -1,3 +1,5 @@
+# 入力は1行で[数値 数値]
+
 il1 = input().split()
 N = int(il1[0])
 K = int(il1[1])
@@ -5,6 +7,3 @@ K = int(il1[1])
 list = [1]
 for i in range(1,N+1):
     list.append(list[-1] * i)
-
-print(list)
-print("N= "+str(N)+" K= "+str(K))
