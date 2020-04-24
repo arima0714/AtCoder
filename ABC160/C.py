ed576fbf8cb011fbc277e83e68d02d4ce598f93b
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-24 11:43:05 +0900

    Cくりあした

--- ABC160/C.py
+++ ABC160/C.py
@@ -5,14 +5,19 @@ K = int(il1[0])
 N = int(il1[1])
 A = [int(i) for i in il2]
 
-import time
 import copy
 
 fixed_A = copy.copy(A)
 
 for i in A:
     fixed_A.append(i+K)
-    print(fixed_A)
-    time.sleep(1)
 
-print(fixed_A)
+ans_list =[]
+
+for i in range(len(A)):
+    # print("i = "+str(i))
+    ans_list.append(fixed_A[i+N-1] - A[i])
+
+ans_list.sort()
+
+print(ans_list[0])
