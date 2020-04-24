33badee9d9ac657f207623113edbf937036b655f
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-24 11:33:10 +0900

    入力されたリストを加工用のリストとして複製した

--- ABC160/C.py
+++ ABC160/C.py
@@ -5,3 +5,14 @@ K = int(il1[0])
 N = int(il1[1])
 A = [int(i) for i in il2]
 
+import time
+import copy
+
+fixed_A = copy.copy(A)
+
+for i in A:
+    fixed_A.append(i+K)
+    print(fixed_A)
+    time.sleep(1)
+
+print(fixed_A)
