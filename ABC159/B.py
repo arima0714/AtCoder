1db64d3a54b47fbce5d46607bca5b28d4bbda400
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-25 22:33:40 +0900

    回文判定関数のplindrome_checker()が期待する動きをしていない

--- ABC159/B.py
+++ ABC159/B.py
@@ -1,5 +1,4 @@
 S = input()
-print(S[0]+S[-1])
 N = len(S)
 print(N)
 
@@ -13,4 +12,9 @@ def plindrome_checker(strings):
             result = False
     return result
 
+str1 = plindrome_checker(S[0:(N-1)//2])
+str2 = plindrome_checker(S[(N-1)//2+1:])
+print("str1 = " + str(str1))
+print("str2 = " + str(str2))
 
+print(S[0:(N-1)//2] +" "+ S[(N-1)//2+1:])
