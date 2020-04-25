69fc932ac852f970186645d5818aa4a55a2edb68
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-25 21:53:52 +0900

    組み合わせを計算する関数を追記した

--- ABC159/A.py
+++ ABC159/A.py
@@ -2,5 +2,11 @@ input_line = input().split()
 N = int(input_line[0])
 M = int(input_line[1])
 
-def kaijou(num):
-    return num * (num+1) / 2
+import math
+
+print("N="+str(N)+" M="+str(M))
+
+def combination(n, r):
+    return math.factorial(n) / math.factorial(n-r) / math.factorial(r)
+
+print(combination(N,M))
