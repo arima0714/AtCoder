981d3340a7a8cacea12e0d4903eaf07f7b657274
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-26 11:37:49 +0900

    題意を満たすコードを追加した

--- ABC159/B.py
+++ ABC159/B.py
@@ -18,5 +18,7 @@ str2 = plindrome_checker(S[(N-1)//2+1:])
 print("str1 = " + str(str1))
 print("str2 = " + str(str2))
 
-print(S[0:(N-1)//2] +" "+ S[(N-1)//2+1:])
-
+if(str1 and str2):
+    print("Yes")
+else:
+    print("No")
