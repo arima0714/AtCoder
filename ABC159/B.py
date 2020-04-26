e778139e94e471a0739b0ec26d9f923f3dbb7542
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-26 11:43:07 +0900

    不要な出力を削除した

--- ABC159/B.py
+++ ABC159/B.py
@@ -1,12 +1,10 @@
 S = input()
 N = len(S)
-print(N)
 
 def plindrome_checker(strings):
     result = True
     length = len(strings)
     for k in range(length):
-        print("strings[k] = "+strings[k]+" , strings[-1 * k] = "+strings[length-1-k])
         if (strings[k] == strings[length-1-k]):
             pass
         else:
@@ -15,8 +13,6 @@ def plindrome_checker(strings):
 
 str1 = plindrome_checker(S[0:(N-1)//2])
 str2 = plindrome_checker(S[(N-1)//2+1:])
-print("str1 = " + str(str1))
-print("str2 = " + str(str2))
 
 if(str1 and str2):
     print("Yes")
