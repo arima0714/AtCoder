c15774040872ab3425aabc92872896523349bb66
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-26 13:49:37 +0900

    全体文のチェックをした

--- ABC159/B.py
+++ ABC159/B.py
@@ -11,10 +11,11 @@ def plindrome_checker(strings):
             result = False
     return result
 
+str0 = plindrome_checker(S)
 str1 = plindrome_checker(S[0:(N-1)//2])
 str2 = plindrome_checker(S[(N-1)//2+1:])
 
-if(str1 and str2):
+if(str0 and str1 and str2):
     print("Yes")
 else:
     print("No")
