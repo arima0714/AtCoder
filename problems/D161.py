104893d97821810b7ff6d7d313dc166fc108f182
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-04-28 12:54:14 +0900

    ルンルン数かどうかを判断する関数を追加した

--- problems/D161.py
+++ problems/D161.py
@@ -1,2 +1,15 @@
 K = int(input())
 
+def check_runrun(number):
+    num_str = str(number)
+    ans = True
+    if(len(num_str) == 1):
+        return ans
+    else:
+        for i in range(len(num_str)-1):
+            if(abs(int(num_str[i]) - int(num_str[i+1])) > 1):
+                ans = False
+                break
+        return ans
+
+print(check_runrun(K))
