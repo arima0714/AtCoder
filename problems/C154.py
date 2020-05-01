e6b9ce6e50066892edd16961a2dbda60109ab001
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-05-01 10:51:12 +0900

    少し効率よさそうな実装にしたがTLE

--- problems/C154.py
+++ problems/C154.py
@@ -1,12 +1,17 @@
-N = input()
+N = int(input())
 A = [int(k) for k in input().split()]
 
 result = True
 
-for char in A:
-    if(A.count(char) != 1):
-        result = False
-        break
+# for char in A:
+#     if(A.count(char) != 1):
+#         result = False
+#         break
+for i in range(N):
+    for j in range(i+1, N):
+        # print("A[i]= "+str(A[i])+", A[j]= "+str(A[j]))
+        if A[i] == A[j]:
+            result = False
 
 if (result == True):
     print("YES")
