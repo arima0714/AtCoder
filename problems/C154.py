23e41d144821c0c3875bbbb01c073269bb4f6692
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-05-01 10:55:38 +0900

    C154解説読んでクリアした

--- problems/C154.py
+++ problems/C154.py
@@ -7,11 +7,17 @@ result = True
 #     if(A.count(char) != 1):
 #         result = False
 #         break
-for i in range(N):
-    for j in range(i+1, N):
-        # print("A[i]= "+str(A[i])+", A[j]= "+str(A[j]))
-        if A[i] == A[j]:
-            result = False
+# for i in range(N):
+#     for j in range(i+1, N):
+#         # print("A[i]= "+str(A[i])+", A[j]= "+str(A[j]))
+#         if A[i] == A[j]:
+#             result = False
+A.sort()
+
+for i in range(N-1):
+    if (A[i] == A[i+1]):
+        result = False
+        break
 
 if (result == True):
     print("YES")
