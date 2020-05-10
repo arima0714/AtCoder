27d7201984ade1f2c9fe2a04cdaffc72f365eb3c
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-05-10 22:51:24 +0900

    Dできませんでした…

--- ABC167/D.py
+++ ABC167/D.py
@@ -3,12 +3,34 @@ N = il[0]
 K = il[1]
 A = [int(k) for k in input().split()]
 A.insert(0,0)
+visited = [-1]*(N+1)
+
+##### ループをしない場合
+
+# k = 0
+# result = 1
+
+
+#### ループをする場合
 
 k = 0
+alpha = 0
 result = 1
+loop_length = 0
+while True:
+    if (visited[result] < 0):
+        visited[result] = k
+        result = A[result]
+        k += 1
+    else:
+        alpha = visited[result]
+        loop_length = k - visited[result]
+        break
 
-while k < K:
+num = (K-alpha)%(loop_length)
+print(num)
+
+while num >= 0:
     result = A[result]
-    k += 1
-
-print(result)
+    num -= 1
+    print(result)
