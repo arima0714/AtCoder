5e2047c8d4bc64652d42726009ca947a2c9f21dc
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-05-31 21:39:28 +0900

    Bをnumpyを使う形式にしたがWA

--- ABC169/B.py
+++ ABC169/B.py
@@ -1,16 +1,9 @@
 N = int(input())
 A = [int(k) for k in input().split()]
 
-result = 1
-
-border = 10 ** 18
-
-for i in A:
-    if result == 0:
-        break
-    result = result * i
-
-if result > border:
+import numpy as np
+result = np.prod(A)
+if (result > 10 ** 18):
     result = -1
 
 print(result)
