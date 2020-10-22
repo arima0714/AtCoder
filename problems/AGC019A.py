6e6f79541269498154abb4f98a11cddcc64bf8a7
arima0714 <32933855+arima0714@users.noreply.github.com>
2020-10-22 03:32:58 +0000

    解説を見たけどクリアした

--- problems/AGC019A.py
+++ problems/AGC019A.py
@@ -6,21 +6,9 @@ H = il[1]
 S = il[2]
 D = il[3]
 
-QList = [Q, 0.25/Q, 0.25]
-HList = [H, 0.5/H, 0.5]
-SList = [S, 1.0/S, 1.0]
-DList = [D, 2.0/D, 2.0]
+single = min([Q*4, H*2, S])
+double = min([single*2, D])
 
-XList = [QList, HList, SList, DList]
+result = (N//2) * double + (N%2) * single
 
-XList = sorted(XList, reverse = True, key=lambda x: x[1])
-
-rest = N
-result = 0
-for x in XList:
-    if[x[2] <= rest]:
-        x_n = rest//x[2]
-        if(x_n != 0):
-            rest = rest - x[2] * x_n
-            result = result + x_n * x[0]
 print(int(result))
