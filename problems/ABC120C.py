a8c076d9e9b8784a63dae54b33fabb7d3aa396ca
arima0714 <32933855+arima0714@users.noreply.github.com>
2020-10-22 06:43:07 +0000

    解説を見てクリアした

--- problems/ABC120C.py
+++ problems/ABC120C.py
@@ -1,30 +1,6 @@
 S = input()
 
-tmp_s = S
-result = 0
-while(("01") in tmp_s):
-    index = tmp_s.find("01")
-    # print(f'index = {index}, tmp_s = {tmp_s}')
-    if(index != 0 and index+2 != len(tmp_s)):
-        tmp_s = tmp_s[:index] + tmp_s[index+2:]
-    elif(index == 0 and index+2 == len(tmp_s)):
-        tmp_s = ""
-    elif(index == 0):
-        tmp_s = tmp_s[index+2:]
-    elif(index+2 == len(tmp_s)):
-        tmp_s = tmp_s[:index]
-    result = result + 2
-while(("10") in tmp_s):
-    index = tmp_s.find("10")
-    # print(f'index = {index}, tmp_s = {tmp_s}')
-    if(index != 0 and index+2 != len(tmp_s)):
-        tmp_s = tmp_s[:index] + tmp_s[index+2:]
-    elif(index == 0 and index+2 == len(tmp_s)):
-        tmp_s = ""
-    elif(index == 0):
-        tmp_s = tmp_s[index+2:]
-    elif(index+2 == len(tmp_s)):
-        tmp_s = tmp_s[:index]
-    result = result + 2
+n_1 = S.count("1")
+n_0 = S.count("0")
 
-print(result)
+print(min([n_1, n_0])*2)
